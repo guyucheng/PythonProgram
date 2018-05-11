f11a0e0f4f6882229f4e7017a37f697feacaf935
辜 裕成 <guyucheng323@outlook.com>
2018-05-11 16:48:00 +0800

    最新学习

---
+++ FunProgranming/PureFunctions.py
@@ -0,0 +1,17 @@
+# Pure function
+def pure_function(x, y):
+    temp = x + 2 * y
+    return temp / (2 * x + y)
+
+
+# Impure function:
+some_list = []
+
+
+def impure(arg):
+    some_list.append(arg)
+
+
+impure("1234")
+print(some_list)
+
---
+++ practice/main.py
@@ -0,0 +1,3 @@
+from practice import myFunc as mf
+
+print(mf.add(3, 4))
